046701ef144520bca0b1a70d07b0a3a91cf1863b
Doug Moscrop <doug.moscrop@gmail.com>
2014-09-26 05:08:33 -0400

    Seems to work.

---
+++ index.js
@@ -0,0 +1,36 @@
+(function () {
+	'use strict';
+	/*global angular, Blob, URL */
+	
+	angular.module('angular.img', [
+	]).directive('httpSrc', function ($http) {
+		return {
+			link: function ($scope, elem, attrs) {
+				function revokeObjectURL() {
+					if ($scope.objectURL) {
+						URL.revokeObjectURL($scope.objectURL);
+					}
+				}
+
+				$scope.$watch('objectURL', function (objectURL) {
+					elem.attr('src', objectURL);
+				});
+
+				$scope.$on('$destroy', function () {
+					revokeObjectURL();
+				});
+
+				attrs.$observe('httpSrc', function (url) {
+					revokeObjectURL();
+
+					if (url) {
+						$http.get(url, { responseType: 'arraybuffer' }).then(function (response) {
+							var blob = new Blob([ response.data ], { type: response.headers('Content-Type') });
+							$scope.objectURL = URL.createObjectURL(blob);
+						});
+					}
+				});
+			}
+		};
+	});
+}());
