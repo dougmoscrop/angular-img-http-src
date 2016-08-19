b25040de1940efd86d7d14c8418d50bb90f2f43e
JiHyung Lee <jhlee.8804@gmail.com>
2016-08-19 22:57:11 +0900

    prevent show same images on img tag

--- index.js
+++ index.js
@@ -5,6 +5,9 @@
 	angular.module('angular.img', [
 	]).directive('httpSrc', ['$http', function ($http) {
 		return {
+			// do not share scope with sibling img tags and parent
+			// (prevent show same images on img tag)
+			scope: {},
 			link: function ($scope, elem, attrs) {
 				function revokeObjectURL() {
 					if ($scope.objectURL) {
